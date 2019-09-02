fb587d0a1aa2edcd1cbcd941d5735d1c0ea44747
eosrapidprod <blocks4days@outlook.com>
2019-09-02 23:43:15 +0800

    whoops spelled adappt wrong

--- build_run_dev.sh
+++ build_run_dev.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-IMAGE_NAME="addapt"
+IMAGE_NAME="adappt"
 CONTAINER_NAME="eosworkflowcon"
-VERSION="1.2.2"
+VERSION="1.2.5"
 IMAGE_FULL_NAME="${IMAGE_NAME}:${VERSION}"
 
 
-docker rm $CONTAINER_NAME
 docker rmi $IMAGE_FULL_NAME
 docker build -t $IMAGE_FULL_NAME .
-docker run --name $CONTAINER_NAME -p 3000:3000 $IMAGE_FULL_NAME
+docker run --rm -p 3000:3000 $IMAGE_FULL_NAME
